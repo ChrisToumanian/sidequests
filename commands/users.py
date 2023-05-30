9ace0c34788053a0073422595799bbcba9e98248
Christopher Toumanian <cctoumanian@gmail.com>
2023-05-30 02:13:32 +0000

    changed remove_user

--- commands/users.py
+++ commands/users.py
@@ -6,29 +6,6 @@ class Users:
         result = db_conn.execute("""
             INSERT INTO players (username) VALUES (%s);
         """, (discord_username,))
-        # Load the configuration file
-        # with open('settings/config.yaml', 'r') as f:
-        #     data = yaml.safe_load(f)
-
-        # # Check if user already exists
-        # for user in data["characters"]:
-        #     if user["id"] == id:
-        #         response = f"User with id {id} already exists"
-        #         return response
-
-        # # Create new user
-        # new_user = {
-        #     "name": name,
-        #     "discord_username": discord_username,
-        #     "id": id
-        # }
-
-        # # Add new user to the existing list
-        # data["characters"].append(new_user)
-
-        # # Write updated data back to the file
-        # with open('settings/config.yaml', 'w') as f:
-        #     yaml.safe_dump(data, f)
 
         if result:
             response = f"Player {discord_username} has been added successfully"
@@ -40,24 +17,8 @@ class Users:
     @staticmethod
     def remove_user(discord_username, db_conn):
         result = db_conn.execute("""
-            DELETE FROM players (username) VALUES (%s);
+            DELETE FROM players WHERE username = %s;
         """, (discord_username,))
-        # # Load the configuration file
-        # with open('settings/config.yaml', 'r') as f:
-        #     data = yaml.safe_load(f)
-
-        # # Find and remove the user with the given name
-        # for i, user in enumerate(data["characters"]):
-        #     if user["discord_username"] == discord_username:
-        #         data["characters"].pop(i)
-        #         break
-        # else:
-        #     response = f"User with username {discord_username} not found"
-        #     return response
-
-        # # Write updated data back to the file
-        # with open('settings/config.yaml', 'w') as f:
-        #     yaml.safe_dump(data, f)
         
         if result:
             response = f"User {discord_username} has been removed successfully"
