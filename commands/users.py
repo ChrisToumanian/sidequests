b47d1aba994ea742e31689b0cbf7aa87461098d5
Christopher Toumanian <cctoumanian@gmail.com>
2023-06-03 11:43:38 +0000

    modified users.py

--- commands/users.py
+++ commands/users.py
@@ -1,51 +1,59 @@
-import yaml
-
 class Users:
     @staticmethod
-    def add_user(discord_username, dnd_beyond_id, db_conn):
+    def add_user(username, dnd_beyond_id, db_conn):
         # Check if user is in database
         res = db_conn.query("""
             SELECT * FROM users WHERE username = %s;
-        """, (discord_username,))
+        """, (username,))
 
         if len(res) > 0:
             # User found, activate
             res = db_conn.execute("""
                 UPDATE users SET activated = true WHERE username = %s;
-            """, (discord_username,))
+            """, (username,))
         else:
             # Create user 
             res = db_conn.execute("""
                 INSERT INTO users (username) VALUES (%s);
-            """, (discord_username,))
+            """, (username,))
 
         # Create or update character
-        Users.add_character(discord_username, dnd_beyond_id, db_conn)
+        Users.add_character(username, dnd_beyond_id, db_conn)
 
-        message = f"Player {discord_username} has been added successfully. Thank you for registering!"
+        message = f"Player {username} has been added successfully. Thank you for registering!"
         return message
 
     @staticmethod
-    def remove_user(discord_username, db_conn):
+    def remove_user(username, db_conn):
         # Check if player is in database
         res = db_conn.query("""
             SELECT * FROM players WHERE username = %s;
-        """, (discord_username,))
+        """, (username,))
 
         if len(res) > 0:
             # Player found, so set to inactive
             res = db_conn.execute("""
                 UPDATE players SET activated = false WHERE username = %s;
-            """, (discord_username,))
-            message = f"{discord_username} has been removed successfully. Thank you for playing!"
+            """, (username,))
+            message = f"{username} has been removed successfully. Thank you for playing!"
         else:
             # Player not found, send warning
-            message = f"No player {discord_username} found. Were you signed up?"
+            message = f"No player {username} found. Were you signed up?"
 
         return message
 
     @staticmethod
-    def add_character(discord_username, dnd_beyond_id, db_conn):
+    def get_current_character_uuid(username, db_conn):
+        res = db_conn.execute("""
+            SELECT current_character_uuid
+            FROM users
+            WHERE username = %s;
+        """, (username))
+
+        return res["character_uuid"]
+
+    @staticmethod
+    def add_character(username, dnd_beyond_id, db_conn):
         res = db_conn.execute("""
             DELETE FROM characters
             WHERE dnd_beyond_id = %s
@@ -68,6 +76,6 @@ class Users:
                 FROM new_character
             )
             WHERE username = %s;
-        """, (dnd_beyond_id, discord_username, dnd_beyond_id, discord_username, discord_username))
+        """, (dnd_beyond_id, username, dnd_beyond_id, username, username))
 
         return bool(res)
